14fdf4cb37cec96f7463517463c9ad0a513d55ee
bhavesh_adhikari <108828490+bhaveshadhikari@users.noreply.github.com>
2023-05-08 23:58:34 +0545

    Update boolean_product.cpp

--- boolean_product.cpp
+++ boolean_product.cpp
@@ -1,38 +1,40 @@
 #include <iostream>
 using namespace std;
-int main() {
-    int r1 , c1;
-    int r2 , c2;
-    cout<<"order for 1st matrix ( m,n ): ";
-    scanf("%d,%d",&r1,&c1);
-    int a[r1][c1];
-    cout <<"enter matrix A :"<<endl;
-    for(int i = 0 ; i < r1; i++)
-      for(int j = 0; j < c1; j++)
-        cin>>a[i][j];
-    x:   
-    cout<<'\n'<<"order for 2nd matrix ( m,n ): ";
-    scanf("%d,%d",&r2,&c2);
-    int b[r2][c2];
-    if ( c1 != r2){
-        cout<<"invalid orientation!!";
-        goto x;
+int main()
+{
+    int r1, c1;
+    cout << "enter order: ";
+    cin >> r1 >> c1;
+    bool a[r1][c1];
+    printf("enter A(%dx%d):\n", r1, c1);
+    for (int i = 0; i < r1; i++)
+        for (int j = 0; j < c1; j++)
+            cin >> a[i][j];
+
+
+    int r2, c2;
+    re_enter:
+    cout << "enter order: ";
+    cin >> r2 >> c2;
+    if (c1 != r2){
+        cout << "invalid orientation!!";
+        goto re_enter;
     }
-    cout <<"enter matrix B :"<<endl;
-    for(int i = 0 ; i < r2; i++)
-      for(int j = 0; j < c2; j++)
-        cin>>b[i][j];
-    
-    cout<<endl<<"Boolean product is :"<<endl;
-    for(int i = 0 ; i < r1; i++){
-        cout<<"| ";
-        for(int j = 0; j < c2; j++){
-            int product=0;
-            for(int k = 0; k < r2; k++)
-               product = product || (a[i][k]&&b[k][j]); 
-            cout <<product<<" ";
-        }
-        cout<<"|"<<'\n';
+
+    bool b[r2][c2];
+    printf("enter B(%dx%d):\n", r2, c2);
+    for (int i = 0; i < r2; i++)
+        for (int j = 0; j < c2; j++)
+            cin >> b[i][j];
+
+    printf("boolean product (%dx%d) : \n", r1, c2);
+    for (int i = 0; i < r1; i++){
+        for (int j = 0; j < c2; j++){
+            bool product = 0;
+            for (int k = 0; k < r2; k++)
+                product = product | (a[i][k] & b[k][j]);
+            cout << product << " ";
+        }cout << char(10);
     }
     return 0;
 }
